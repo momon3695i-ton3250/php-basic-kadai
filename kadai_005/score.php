5784d76bc04840862eb133fdbc35e6a9c82e8936
momon3695i-ton3250 <attin100ko@gmail.com>
2024-08-19 22:19:58 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>テストの平均点を計算しよう</title>
+</head>
+<body>
+<p>
+
+<?php
+
+$score1 = 80;
+$score2 = 50;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 平均点
+$score_average = $total_score / 10;
+echo $score_average;
+
+?>
+
+</p>
+</body>
+</html>
+
